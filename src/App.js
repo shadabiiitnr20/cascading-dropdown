19bc2accf12452e5a9c0f1e4d244c65af92eaee7
Md. Shadab Hussain <shadab.iiitnr20@gmail.com>
2023-10-22 14:12:56 +0530

    simple cascading dropdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { states, cities } from "./constants/data";
 
 function App() {
+  const [selectedState, setSelectedState] = useState(states[0]);
+
+  // console.log(selectedState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h2 className="underline text-xl font-bold m-4">Cascading Dropdown</h2>
+      <div>
+        <select
+          className="m-4 border border-solid border-black w-14 rounded-lg"
+          onChange={(e) => setSelectedState(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          {states.map((state, i) => {
+            return <option key={i}>{state}</option>;
+          })}
+        </select>
+        {selectedState && (
+          <>
+            <select className="m-4 border border-solid border-black w-28 rounded-lg">
+              {cities[selectedState].map((city, i) => {
+                return <option key={i}>{city}</option>;
+              })}
+            </select>
+          </>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/constants/data.js
@@ -0,0 +1,6 @@
+export const states = ["CG", "UP", "MH"];
+export const cities = {
+  CG: ["Raipur", "Bhilai", "Bilaspur"],
+  UP: ["Lucknow", "Banaras", "Kanpur"],
+  MH: ["Mumbai", "Pune", "Nagpur"],
+};
